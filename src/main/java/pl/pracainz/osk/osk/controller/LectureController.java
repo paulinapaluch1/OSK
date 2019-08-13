fd719b9592673cb44880ac0d16eaa16c3a152257
IwonaPoleszak <ivonapoleszak@gmail.com>
2019-08-13 19:45:23 +0200

    zrobienie stron car i lecture

---
+++ src/main/java/pl/pracainz/osk/osk/controller/LectureController.java
@@ -0,0 +1,76 @@
+package pl.pracainz.osk.osk.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import pl.pracainz.osk.osk.dao.CategoryRepository;
+import pl.pracainz.osk.osk.dao.CourseRepository;
+import pl.pracainz.osk.osk.dao.InstructorRepository;
+import pl.pracainz.osk.osk.dao.LectureRepository;
+import pl.pracainz.osk.osk.entity.Lecture;
+
+@Controller
+@RequestMapping("/lectures")
+public class LectureController {
+	private LectureRepository lectureRepository;
+	private CourseRepository courseRepository;
+	private CategoryRepository categoryRepository;
+	private InstructorRepository instructorRepository;
+	
+
+	public LectureController(LectureRepository repository, CourseRepository course, CategoryRepository category, InstructorRepository instructor) {
+		this.lectureRepository = repository;
+		this.courseRepository = course;
+		this.categoryRepository = category;
+		this.instructorRepository = instructor;
+	}
+
+	@GetMapping("/list")
+	public String listLectures(Model theModel) {
+
+		List<Lecture> theLectures = lectureRepository.findAll();
+
+		theModel.addAttribute("lectures", theLectures);
+
+		return "adminViews/adminLectures/lectures";
+	}
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		Lecture theLecture = new Lecture();
+
+		theModel.addAttribute("lecture", theLecture);
+		theModel.addAttribute("courses", courseRepository.findAll());
+		theModel.addAttribute("instructors", instructorRepository.findAll());
+		theModel.addAttribute("categories", categoryRepository.findAll());
+		return "adminViews/adminLectures/lectureForm";
+	}
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("id_lecture") int id, Model theModel) {
+
+		Optional<Lecture> theLecture = lectureRepository.findById(id);
+		theModel.addAttribute("lecture", theLecture);
+		theModel.addAttribute("courses", courseRepository.findAll());
+		theModel.addAttribute("instructors", instructorRepository.findAll());
+		theModel.addAttribute("categories", categoryRepository.findAll());
+		
+		return "adminViews/adminLectures/lectureForm";
+	}
+
+	@PostMapping("save")
+	public String saveLecture(@ModelAttribute("lecture") Lecture theLecture) {
+		lectureRepository.save(theLecture);
+
+		return "redirect:/lectures/list";
+	}
+
+}
