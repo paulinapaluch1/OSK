2378b02014458b5c7906d9b321f994dacf21c369
Paulina <paulina.paluch1@pollub.edu.pl>
2019-08-17 15:06:59 +0200

    poprawka

--- src/main/java/pl/pracainz/osk/osk/controller/MyErrorController.java
+++ src/main/java/pl/pracainz/osk/osk/controller/MyErrorController.java
@@ -17,25 +17,5 @@ public class MyErrorController implements ErrorController  {
         return "/error";
     }
     
-    
-   /* 
-    @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-         
-        if (status != null) {
-            Integer statusCode = Integer.valueOf(status.toString());
-         
-            if(statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "error-404";
-            }
-            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "error-500";
-            }
-        }
-        return "error";
-    }
-    
-    
-    */
+   
 }
