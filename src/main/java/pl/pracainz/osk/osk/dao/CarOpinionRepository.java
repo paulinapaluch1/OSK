4edee1bd7b9281f5ee95982f0ee5ab7a1d4fab49
IwonaPoleszak <ivonapoleszak@gmail.com>
2019-08-21 21:55:02 +0200

    opinie dla samochodow - cos jeszcze nie dziala

---
+++ src/main/java/pl/pracainz/osk/osk/dao/CarOpinionRepository.java
@@ -0,0 +1,9 @@
+package pl.pracainz.osk.osk.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pl.pracainz.osk.osk.entity.CarOpinion;
+
+public interface CarOpinionRepository extends JpaRepository<CarOpinion, Integer>{
+
+}
