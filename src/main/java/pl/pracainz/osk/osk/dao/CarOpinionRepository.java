16e08ea95c8341ddab7d7aecdab95078e0e65054
IwonaPoleszak <ivonapoleszak@gmail.com>
2019-08-30 21:54:14 +0200

    poprawione opinie, jazdy oraz wyklady

--- src/main/java/pl/pracainz/osk/osk/dao/CarOpinionRepository.java
+++ src/main/java/pl/pracainz/osk/osk/dao/CarOpinionRepository.java
@@ -1,9 +1,13 @@
 package pl.pracainz.osk.osk.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pl.pracainz.osk.osk.entity.CarOpinion;
 
 public interface CarOpinionRepository extends JpaRepository<CarOpinion, Integer>{
 
+	public List<CarOpinion> findByDeleted(int deleted);
+	public List<CarOpinion> findByStatus(String status);
 }
