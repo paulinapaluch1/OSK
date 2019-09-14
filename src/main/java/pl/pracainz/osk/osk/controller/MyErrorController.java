13b5495f7795dc43823a31ce6ff5ec1f798c3cd8
Paulina <paulina.paluch1@pollub.edu.pl>
2019-09-14 21:41:32 +0200

    logowanie

--- src/main/java/pl/pracainz/osk/osk/controller/MyErrorController.java
+++ src/main/java/pl/pracainz/osk/osk/controller/MyErrorController.java
@@ -2,11 +2,16 @@ package pl.pracainz.osk.osk.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyErrorController implements ErrorController  {
- 
+
+	@Override
+	public String getErrorPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+ /*
     @RequestMapping("/error")
     public String handleError() {
         return "error";
@@ -16,6 +21,6 @@ public class MyErrorController implements ErrorController  {
     public String getErrorPath() {
         return "/error";
     }
-    
+    */
    
 }
