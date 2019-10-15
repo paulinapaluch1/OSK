b78f5897ab49de4c9ded24c5fd80d7c5c5926d0a
IwonaPoleszak <ivonapoleszak@gmail.com>
2019-10-15 20:27:22 +0200

    wysylanie maila

---
+++ src/main/java/pl/pracainz/osk/osk/controller/SimpleEmailExampleController.java
@@ -0,0 +1,33 @@
+package pl.pracainz.osk.osk.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+ 
+@Controller
+public class SimpleEmailExampleController {
+ 
+    @Autowired
+    public JavaMailSender emailSender;
+ 
+    @ResponseBody
+    @RequestMapping("/sendEmail")
+    public String sendSimpleEmail() {
+ 
+        // Create a Simple MailMessage.
+        SimpleMailMessage message = new SimpleMailMessage();
+         
+        message.setTo("ivonapoleszak@gmail.com");
+        message.setSubject("[OSK] Dane do logowania");
+        message.setText("Witaj, Twoje dane do logowania to: login: , hasło: ");
+ 
+        // Send Message!
+        this.emailSender.send(message);
+ 
+        return "Email Sent!";
+    }
+ 
+}
