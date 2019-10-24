873d12c370ac0a6119c5d1304137a2a45967fc9a
IwonaPoleszak <ivonapoleszak@gmail.com>
2019-10-24 05:27:03 +0200

    login w mailu + ulepszenie sredniej + dodanie sredniej w reszczie miejsc

--- src/main/java/pl/pracainz/osk/osk/controller/SimpleEmailExampleController.java
+++ src/main/java/pl/pracainz/osk/osk/controller/SimpleEmailExampleController.java
@@ -3,35 +3,49 @@ package pl.pracainz.osk.osk.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import pl.pracainz.osk.osk.dao.StudentRepository;
+import pl.pracainz.osk.osk.dao.UserRepository;
+import pl.pracainz.osk.osk.entity.Student;
+import pl.pracainz.osk.osk.entity.User;
  
 @Controller
 public class SimpleEmailExampleController {
- 
+	private UserRepository userRepository;
+	private StudentRepository studentRepository;
+	private PasswordEncoder encoder;
     @Autowired
     public JavaMailSender emailSender;
  
     @ResponseBody
     @RequestMapping("/sendEmail")
-    public String sendSimpleEmail(@ModelAttribute("email") String email) {
- 
-    	String content = "Witaj! \nTwoje dane do logowania to:\n login: \n hasło: "; 
-        // Create a Simple MailMessage.
-        SimpleMailMessage message = new SimpleMailMessage();
+    public String sendSimpleEmail(@ModelAttribute("email") String email, @ModelAttribute("login") String login) {
+
+    	//User user = userRepository.getOne(new Long(13));
+    	//User user = userRepository.findByUsername("student3");
+    	//Student theStudent = studentRepository.getOne(2);
+
+    	//String password = encoder.encode(user.getPassword());
+    	//String login = theStudent.getLogin();
+    	String password = "haslo";
+    	String content = "Witaj! \nTwoje dane do logowania to:\nlogin: " + login + "\nhasło: " + password; 
+
+    	SimpleMailMessage message = new SimpleMailMessage();
          
 
         message.setTo(email);
-
         message.setSubject("[OSK] Dane do logowania");
         message.setText(content);
  
-        // Send Message!
+
         this.emailSender.send(message);
  
-        return "Email Sent!";
+        return "redirect:/students/list";
     }
  
 }
