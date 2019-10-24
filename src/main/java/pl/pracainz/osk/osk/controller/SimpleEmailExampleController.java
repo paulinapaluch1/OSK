54d2a9c4d38eed8e5d5c5aa55ef6ed45a434989e
Paulina <paulina.paluch1@pollub.edu.pl>
2019-10-24 19:44:45 +0200

    email i progress w edycji wykladu

--- src/main/java/pl/pracainz/osk/osk/controller/SimpleEmailExampleController.java
+++ src/main/java/pl/pracainz/osk/osk/controller/SimpleEmailExampleController.java
@@ -3,35 +3,51 @@ package pl.pracainz.osk.osk.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import pl.pracainz.osk.osk.PasswordGenerator;
+import pl.pracainz.osk.osk.dao.StudentRepository;
+import pl.pracainz.osk.osk.dao.UserPrincipalDetailsService;
+import pl.pracainz.osk.osk.dao.UserRepository;
+import pl.pracainz.osk.osk.entity.Student;
+import pl.pracainz.osk.osk.entity.User;
  
 @Controller
 public class SimpleEmailExampleController {
  
     @Autowired
     public JavaMailSender emailSender;
+    
+    @Autowired
+    PasswordEncoder encoder;
  
-    @ResponseBody
+    @Autowired
+    StudentRepository studentRepository;
+    
+    @Autowired
+    UserRepository userRepository;
+    
+    
     @RequestMapping("/sendEmail")
-    public String sendSimpleEmail(@ModelAttribute("email") String email) {
- 
-    	String content = "Witaj! \nTwoje dane do logowania to:\n login: \n hasło: "; 
-        // Create a Simple MailMessage.
+    public String sendSimpleEmail(@ModelAttribute("email") String email, @RequestParam("id") int id) {
+        Student student = studentRepository.getOne(id);
+        String password = PasswordGenerator.generatePassword(20);
+        User user  = userRepository.findById(student.getUserId());
+        user.setPassword(encoder.encode(password));
+		userRepository.save(user);
+		
+		String content = "Witaj " + student.getName() + "! \nTwoje dane do logowania to:\n login: " + student.getLogin() + " \n hasło: "+ password; 
         SimpleMailMessage message = new SimpleMailMessage();
-         
-
         message.setTo(email);
-
         message.setSubject("[OSK] Dane do logowania");
         message.setText(content);
  
-        // Send Message!
         this.emailSender.send(message);
- 
-        return "Email Sent!";
+		return "redirect:students/list";
     }
  
 }
