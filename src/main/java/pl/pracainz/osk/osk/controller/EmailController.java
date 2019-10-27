4fb7cd8568ad7f98ad580fd83f080622b5365e01
Paulina <paulina.paluch1@pollub.edu.pl>
2019-10-27 16:45:23 +0100

    proba zwiekszenia liczy godzin po rezrwacji jazdy

--- src/main/java/pl/pracainz/osk/osk/controller/EmailController.java
+++ src/main/java/pl/pracainz/osk/osk/controller/EmailController.java
@@ -11,14 +11,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import pl.pracainz.osk.osk.PasswordGenerator;
 import pl.pracainz.osk.osk.dao.StudentRepository;
-import pl.pracainz.osk.osk.dao.UserPrincipalDetailsService;
 import pl.pracainz.osk.osk.dao.UserRepository;
 import pl.pracainz.osk.osk.entity.Student;
 import pl.pracainz.osk.osk.entity.User;
+import pl.pracainz.osk.osk.entity.UserPrincipal;
  
 @Controller
-public class SimpleEmailExampleController {
+public class EmailController {
  
+	int GENERATED_PASSWORD_SIZE = 20;
+	
     @Autowired
     public JavaMailSender emailSender;
     
@@ -34,18 +36,17 @@ public class SimpleEmailExampleController {
     
     @RequestMapping("/sendEmail")
     public String sendSimpleEmail(@ModelAttribute("email") String email, @RequestParam("id") int id) {
-        Student student = studentRepository.getOne(id);
+    	Student student = studentRepository.getOne(id);
         String password = PasswordGenerator.generatePassword(20);
         User user  = userRepository.findById(student.getUserId());
         user.setPassword(encoder.encode(password));
 		userRepository.save(user);
-		
 		String content = "Witaj " + student.getName() + "! \nTwoje dane do logowania to:\n login: " + student.getLogin() + " \n hasło: "+ password; 
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(email);
         message.setSubject("[OSK] Dane do logowania");
         message.setText(content);
- 
+        
         this.emailSender.send(message);
 		return "redirect:students/list";
     }
---
+++ src/main/java/pl/pracainz/osk/osk/dao/ParticipantRepository.java
@@ -0,0 +1,12 @@
+package pl.pracainz.osk.osk.dao;
+
+import javax.mail.Part;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pl.pracainz.osk.osk.entity.Participant;
+
+public interface ParticipantRepository extends JpaRepository<Participant, Integer> {
+
+
+}
